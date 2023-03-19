274d1bf04e6d0f318be41ab13316103c9c1c1f26
kubarada <jakub.rd@seznam.cz>
2023-03-19 11:32:22 +0100

    úprava metody voice_asimilation

--- rules.py
+++ rules.py
@@ -10,10 +10,10 @@
 
 import re
 
-BASIC_RULES = {'á':'A', 'y':'i', 'ň':'N', 'í':'I', 'é':'E','ó':'O','ů':'ú', 'ú':'U', 'š':'S', 'ch':'x', 'ř':'R', 
-'ť':'T', 'ď':'D', 'ň':'J', 'č':'C', 'dz':'w', 'dž':'W', 'ch|':'G', 'm|':'H', 'ž':'Z', 'ou':'y', 'au':'Y', 'eu':'F'}
+BASIC_RULES = {'á':'A', 'y':'i', 'í':'I', 'é':'E','ó':'O','ů':'ú', 'ú':'U', 'š':'S', 'ř':'R',
+'ť':'T', 'ď':'D', 'ň':'J', 'č':'C', 'ž':'Z', 'ou':'y', 'au':'Y', 'eu':'F'}
 
-VOCALS =  ['a', 'e', 'i' ,'o', 'u', 'á' ,'é', 'í','ó', 'ů', 'ú']
+VOCALS = ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ů', 'ú']
 
 CONSTANT = ['b', 'c', 'č', 'd', 'ď', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ň', 'p', 'q', 'r', 'ř', 's', 'š', 't', 'ť', 'v','w', 'x', 'y', 'z', 'ž'] 
 
@@ -24,13 +24,16 @@ BPV = ['b', 'p', 'v']
 
 DTNM = ['d', 't', 'n', 'm']
 
-SPLIT_ON = ['.', '?', '!', '-' , '_']
+SPLIT_ON = ['.', '?', '!', '-', '_']
 
-VOICE_CONSTANT = {'b':'p', 'd':'t', 'ď':'ť', 'g' :'k', 'v':'f', 'z':'s', 'ž':'š', 'h':'x', 'ř':'Q', 'dz':'c', 'dž':'č'}
+VOICE_CONSTANT = {'b':'p', 'd':'t', 'ď':'ť', 'g' :'k', 'v':'f', 'z':'s', 'ž':'š', 'ř':'Q', 'dz':'c', 'dž':'č'}
+
+VOICE_CONSTANTx = {'b':'pp', 'd':'tt', 'ď':'ťť', 'g' :'kk', 'v':'ff', 'z':'ss', 'ž':'šš', 'ř':'QQ', 'dz':'cc', 'dž':'čč'}
+VOICE_CONSTANTxx = {'pp':'p', 'tt':'t', 'ťť':'ť', 'kk' :'k', 'ff':'f', 'ss':'s', 'šš':'š', 'xx':'x', 'QQ':'Q', 'cc':'c', 'čč':'č'}
 
 VOICLESS_CONSTANT = {v: k for k, v in VOICE_CONSTANT.items()} # swap keys and vals in VOICE_CONSTANT
 
-UNIQUE_CONSTANT = ['m', 'n', 'ň', 'l','r', 'j']
+UNIQUE_CONSTANT = ['m', 'n', 'ň', 'l', 'r', 'j']
 
 
 def preproces(text):
@@ -40,6 +43,7 @@ def preproces(text):
     splited = '|$|' + splited.replace(',', '|#')
     splited = splited + '$|'
     splited = splited.replace('\n', '')
+    splited = splited.replace('ch', 'x')
     return splited
 
 
@@ -72,19 +76,32 @@ def constant_tran(text):
     return text
 
 def voice_asimilation(text):
-    for i in VOICE_CONSTANT.keys():
+    # tady řeším ty základní srandy (rovnice 2.26)
+    for i in VOICE_CONSTANTx.keys():
         for j in VOICLESS_CONSTANT.keys():
-            text = text.replace(i + j, VOICE_CONSTANT[i]+j)
-            text = text.replace(i + '|' + j, VOICE_CONSTANT[i]+ '|' + j)
+            text = text.replace(i + j, VOICE_CONSTANTx[i]+j)
+            text = text.replace(i + '|' + j, VOICE_CONSTANTx[i]+ '|' + j)
+            text = text.replace(i + '|$|', VOICE_CONSTANTx[i] + '|$|')
         for k in UNIQUE_CONSTANT:
-            text = text.replace(i + '|' + k, VOICE_CONSTANT[i]+ '|' + k)
+            text = text.replace(i + '|' + k, VOICE_CONSTANTx[i]+ '|' + k)
         for q in VOCALS:
-            text = text.replace(i + '|' + q, VOICE_CONSTANT[i]+ '|' + q)
-        text = text.replace(i + '|#|', VOICE_CONSTANT[i]+'|#|')
+            text = text.replace(i + '|' + q, VOICE_CONSTANTx[i]+ '|' + q)
+
+        # tady řeším ř na Q (rovnice 2.29)
+        for z in VOICLESS_CONSTANT:
+            text = text.replace(z + 'ř', z + 'Q')
+        text = text.replace(i + '|#|', VOICE_CONSTANTx[i]+'|#|')
+
+    # (rovnice 2.26)
     for i in VOICLESS_CONSTANT.keys():
-        for j in VOICE_CONSTANT.keys():
+        for j in VOICE_CONSTANTx.keys():
             text = text.replace(i + j, VOICLESS_CONSTANT[i]+j)
-            text = text.replace(i + '|' + j, VOICLESS_CONSTANT[i]+ '|' + j)
+            text = text.replace(i + '|' + j, VOICLESS_CONSTANT[i] + '|' + j)
+
+    # vifikundace, aby se mi to nepřepisovalo
+    for key in VOICE_CONSTANTxx.keys():
+        text = text.replace(key, VOICE_CONSTANTxx[key])
+
 
     return text
 
